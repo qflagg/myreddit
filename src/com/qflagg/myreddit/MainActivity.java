8d8771f2a4683109bc91f2148575cb73dfb87020
qflagg <qflagg@hotmail.com>
2014-04-01 22:59:57 -0700

    working with cards-ui library

--- src/com/qflagg/myreddit/MainActivity.java
+++ src/com/qflagg/myreddit/MainActivity.java
@@ -1,24 +1,37 @@
 package com.qflagg.myreddit;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 import android.support.v4.app.FragmentActivity;
 
+import com.afollestad.cardsui.Card;
+import com.afollestad.cardsui.CardAdapter;
+import com.afollestad.cardsui.CardHeader;
+import com.afollestad.cardsui.CardListView;
+
 public class MainActivity extends FragmentActivity {
-    
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        addFragment();
-    }
-    
-    void addFragment(){
-        getSupportFragmentManager()
-            .beginTransaction()
-            .add(R.id.fragments_holder
-                 , PostsFragment.newInstance("askreddit"))
-            .commit();
-    }
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		CardListView cardsList = (CardListView) findViewById(R.id.cardsList);
+		CardAdapter cardsAdapter = new CardAdapter(this, 0);
+		cardsList.setAdapter(cardsAdapter);
+
+		cardsAdapter.add(new CardHeader("Subreddit"));
+		cardsAdapter.add(new Card("Title 1", "Author 1"));
+		cardsAdapter.add(new Card("Title 2","Author 2"));
+		cardsAdapter.add(new Card("Title 3", "Author 3").setLayout(R.layout.list_item_card));
+		
+		
+	}
 }
+
+// void addFragment(){
+// getSupportFragmentManager()
+// .beginTransaction()
+// .add(R.id.fragments_holder
+// , PostsFragment.newInstance("askreddit"))
+// .commit();
+// }
